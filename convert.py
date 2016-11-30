381aeec8354d8400506677902569d5273e84fbeb
xtofl <kristoffel.pirard@gmail.com>
2016-11-30 19:49:00 +0100

    import CODE from activestate

--- convert.py
+++ convert.py
@@ -1,10 +1,32 @@
 import sys
 
 def to_caesar(text, key=('s', 12)):
+    """
+    >>> to_caesar("abc", key=('a', 0))
+    'abc'
+    """
     return text
 
 def to_morse(text):
-    return "  ".join(word for word in text.split())
+    CODE = {
+        'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+     	'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
+		    
+    convert = lambda w: str([CODE[c] for c in w])
+    return "  ".join(convert(word.upper()) for word in text.split())
 
 def convert(path):
     text = "\n".join(open(path).readlines())

d7e2aa73fa588d8ad1f3e336c92bfe61cd880b83
xtofl <kristoffel.pirard@gmail.com>
2016-12-01 19:47:01 +0100

    a version of caesar code

--- convert.py
+++ convert.py
@@ -1,11 +1,23 @@
 import sys
+import string
 
 def to_caesar(text, key=('s', 12)):
     """
-    >>> to_caesar("abc", key=('a', 0))
-    'abc'
+    >>> to_caesar("abc", key=('a', 2))
+    'bcd'
+    >>> to_caesar("abc", key=('b', 3))
+    'bcd'
+    >>> to_caesar("safe: tegen", key=('s', 23))
+    'weji: xikir'
     """
-    return text
+    alphabet = string.ascii_lowercase
+    # shifted('s') == ascii(12)
+    # s + shift = 12
+    # shift = 12 - ord(s)
+    shift = (key[1]-1) - (ord(key[0]) - ord('a'))
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    table = str.maketrans(alphabet, shifted_alphabet)
+    return text.translate(table)
 
 def to_morse(text):
     """
@@ -31,12 +43,17 @@ def to_morse(text):
         '9': '----.' 
         }
 		    
-    convert = lambda w: "  ".join([CODE[c] for c in w])
+    def encode(c):
+        if c in CODE:
+            return CODE[c]
+        else:
+            return c
+    convert = lambda w: "  ".join([encode(c) for c in w])
     return "  /  ".join(convert(word.upper()) for word in text.split())
 
 def convert(path):
     text = "\n".join(open(path).readlines())
-    caesar = to_caesar(text)
+    caesar = "dit is code s twaalf\n" + to_caesar(text, key=('s', 12))
     morse = to_morse(caesar)
     print(caesar)
     print(morse)
